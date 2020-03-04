74ddfcc8af7a934002d87705ed415f8c5d6e4f2e
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-03-04 17:47:35 -0300

    subindo a cifra de cesar

--- cifraDeCesar.js
+++ cifraDeCesar.js
@@ -2,15 +2,14 @@
 /* ------ Normal ------
 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
 a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z
- */
+*/
 
-/*------ ASC ||
+/*------ ASC || ---------
 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90
 A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z
- */
+*/
 
 let textoParaCifrar = "ricardo carvalho".trim();
-
 let rotation = 27;
 let cifra = textoParaCifrar.toUpperCase();
 let textoCifrado = [];
